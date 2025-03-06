19239533e6840b9f347531389e4aad56d77fe84e
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-03-06 11:23:27 +0300

    Edits index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression for divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function for square
+  const square = (num) => num * num;
+  
+  // Arrow function for add
+  const add = (a, b) => a + b;
+  
+  // Testing the functions
+  console.log(divide()); // Outputs: 20
+  console.log(square(4)); // Outputs: 16
+  console.log(add(5, 10)); // Outputs: 15
